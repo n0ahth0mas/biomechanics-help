a60c4c507ddc2f3c822eb86448dbd66f7e3ab865
Jack Riley <jriley9000@gmail.com>
2020-02-04 17:15:59 -0800

    new landing view

--- app.py
+++ app.py
@@ -80,9 +80,9 @@ def internal_error(error):
     return render_template('errors/500.html'), 500
 
 
-@app.errorhandler(404)
-def not_found_error(error):
-    return render_template('errors/404.html'), 404
+#@app.errorhandler(404)
+#def not_found_error(error):
+#    return render_template('errors/404.html'), 404
 
 if not app.debug:
     file_handler = FileHandler('error.log')
