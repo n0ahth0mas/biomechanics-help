12707b68c09e395edb1e6e025436dad1304c52dc
Jack Riley <jriley9000@gmail.com>
2020-02-20 07:11:29 -0800

    student authentication and login required for student views

--- app.py
+++ app.py
@@ -10,18 +10,31 @@ from forms import *
 import os
 import sqlite3
 import hashlib
-
+from flask import g, redirect, abort
+from flask import session
 # ----------------------------------------------------------------------------#
 # App Config.
 # ----------------------------------------------------------------------------#
 
 app = Flask(__name__)
 app.config.from_object('config')
-db = sqlite3.connect('login.db')
 
-cur = db.cursor()
+pathToDB = os.path.abspath("studentlogin.db")
+print(pathToDB)
+
+home_url = "http://127.0.0.1:5000/"
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(pathToDB)
+    return db
 
 
+def query_db(query, args=(), one=False):
+    cur = get_db().execute(query, args)
+    rv = cur.fetchall()
+    cur.close()
+    return (rv[0] if rv else None) if one else rv
 # Automatically tear down SQLAlchemy.
 '''
 @app.teardown_request
@@ -30,9 +43,8 @@ def shutdown_session(exception=None):
 '''
 
 # Login required decorator.
-'''
+
 def login_required(test):
-    @wraps(test)
     def wrap(*args, **kwargs):
         if 'logged_in' in session:
             return test(*args, **kwargs)
@@ -40,7 +52,7 @@ def login_required(test):
             flash('You need to login first.')
             return redirect(url_for('login'))
     return wrap
-'''
+
 
 
 # ----------------------------------------------------------------------------#
@@ -54,16 +66,28 @@ def home():
 
 @app.route('/student-quiz')
 def student_quiz():
-    return render_template('pages/placeholder.student.quiz.html')
+    try:
+        session["logged_in"]
+        return render_template('pages/placeholder.student.quiz.html')
+    except KeyError:
+        return abort(401)
 
 
 @app.route('/student-short')
 def student_short():
-    return render_template('pages/placeholder.student.short.html')
+    try:
+        session["logged_in"]
+        return render_template('pages/placeholder.student.short.html')
+    except KeyError:
+        return abort(401)
 
 @app.route('/info-slide')
 def infoSlide():
-    return render_template('layouts/infoSlide.html')
+    try:
+        session["logged_in"]
+        return render_template('layouts/infoSlide.html')
+    except KeyError:
+        return abort(401)
 
 @app.route('/glossary-template')
 def glossaryTemplate():
@@ -94,8 +118,15 @@ def login():
 def studentLogin():
     form = ClassCodeForm()
     if form.validate_on_submit():
-        userCode = form.data["classCode"]
-        print("thinks we submitted the form: " + userCode)
+        user_code = form.data["classCode"]
+
+        class_object = query_db('select * from ClassCodes where ClassCode = ?',
+                                [user_code], one=True)
+        if class_object is None:
+            print('No such class')
+        else:
+            session["logged_in"] = True
+            return redirect(home_url + "student-quiz")
     return render_template('forms/classcode.html', form=form)
 
 @app.route('/new-professor-account', methods=['GET', 'POST'])
@@ -108,12 +139,19 @@ def new_prof_acc():
         h = hashlib.md5(password.encode())
         passhash = h.hexdigest()
         print(passhash)
-    return render_template('forms/NewProfAccount.html', form=form)
 
-@app.route('/register')
-def register():
-    form = RegisterForm(request.form)
-    return render_template('forms/register.html', form=form)
+
+        cursor.execute(
+            'insert into login (username,email,password) values (?,?,?)',
+            (
+                form.data["fullName"],
+                passhash,
+                form.data["email"]
+            )
+        )
+
+        print("past insertion")
+    return render_template('forms/NewProfAccount.html', form=form)
 
 
 @app.route('/forgot')
@@ -131,9 +169,13 @@ def internal_error(error):
     return render_template('errors/500.html'), 500
 
 
-# @app.errorhandler(404)
-# def not_found_error(error):
-#    return render_template('errors/404.html'), 404
+@app.errorhandler(404)
+def not_found_error(error):
+   return render_template('errors/404.html'), 404
+
+@app.errorhandler(401)
+def not_authed_error(error):
+   return render_template('errors/401.html'), 404
 
 if not app.debug:
     file_handler = FileHandler('error.log')
