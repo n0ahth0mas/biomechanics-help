82f6716ace998b95a27fe42b77d42fd6966a70c3
Noah <npthomas@pugetsound.edu>
2020-03-29 13:14:07 -0700

    building timer functionality

--- static/js/section.js
+++ static/js/section.js
@@ -1,8 +1,20 @@
+//HTML items
+const scoreCounter = document.getElementById("scoreCounter");
+const finalScore = document.getElementById("quiz-final-score");
+
+//timer Setup
+let TIMER;
+const questionTime = 60; // 15s
+const timeGauge = document.getElementById("timer_gauge");
+let gaugeWidth = 200;
+const gaugeUnit = gaugeWidth / questionTime;
+const counter = document.getElementById("timer_time");
+var count = 1;
 
 /* QUIZ */
 function submitMultipleChoiceAnswer(button, truthValue, reason, buttonID){
     if(truthValue === 'True'){
-        document.getElementById("scoreCounter").innerHTML = String(Number(String(document.getElementById("scoreCounter").innerHTML)) + 1);
+        scoreCounter.innerHTML = String(Number(String(document.getElementById("scoreCounter").innerHTML)) + 1);
         alert("correct!");
     }else{
         document.getElementById(buttonID).className = "disabled";
@@ -14,7 +26,7 @@ function submitMultipleChoiceAnswer(button, truthValue, reason, buttonID){
 function submitShortAnswer(input, answer){
 
     if(String(answer).includes(String(input.previousElementSibling.value))){
-        document.getElementById("scoreCounter").innerHTML = String(Number(String(document.getElementById("scoreCounter").innerHTML)) + 1);
+        scoreCounter.innerHTML = String(Number(String(document.getElementById("scoreCounter").innerHTML)) + 1);
         alert("correct!");
     }else{
         alert("wrong!");
@@ -27,12 +39,21 @@ function nextQuestion(currentQuestion){
         currentQuestion.style.display = "none";
         currentQuestion.nextElementSibling.style.display = "block";
     }else{
-        document.getElementById("quiz-final-score").parentElement.style.display = "block";
-        document.getElementById("quiz-final-score").innerHTML = String((parseFloat(document.getElementById("scoreCounter").innerHTML) / parseFloat(document.getElementById("quizLength").innerHTML)) * 100) + "%";
+        finalScore.parentElement.style.display = "block";
+        finalScore.innerHTML = String((parseFloat(document.getElementById("scoreCounter").innerHTML) / parseFloat(document.getElementById("quizLength").innerHTML)) * 100) + "%";
     }
 }
 
 function startQuiz(){
     document.getElementById("questions").style.display = "flex"
     document.getElementById("header").style.display = "none"
+    TIMER = setInterval(renderCounter, 1000)
+}
+
+function renderCounter(){
+    if(count<questionTime){
+        counter.innerHTML = count;
+        timeGauge.style.width = count * gaugeUnit + "px";
+        count++;
+    }
 }
