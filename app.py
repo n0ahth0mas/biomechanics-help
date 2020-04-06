0316d0fab5e593395f9da4c54ea2c4d5d76d9180
rbarraza21 <rbarraza@pugetsound.edu>
2020-04-05 19:04:58 -0700

    Update app.py

--- app.py
+++ app.py
@@ -880,7 +880,7 @@ if not app.debug:
 # Default port:
 if __name__ == '__main__':
     app.secret_key = 'xxxxyyyyyzzzzz'
-    app.run(host='149.165.157.94', port=int(os.environ.get('PORT','5000')))
+    app.run(host="149.165.157.94", port=5000)
 
 # Or specify port manually:
 '''
