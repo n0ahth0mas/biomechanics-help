37295406a3c7bd9a6c0b1a226b7274b54c6b6c76
Noah <npthomas@pugetsound.edu>
2020-04-13 14:31:04 -0700

    Video Slideshow working

--- static/js/section.js
+++ static/js/section.js
@@ -131,3 +131,34 @@ function nextQuestion(){
         document.getElementById("next").style.display = "flex";
     }
 }
+
+/* SLIDESHOW */
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("page_vid");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+      slides[i].pause();
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
