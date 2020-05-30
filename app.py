5a096611fc68a44c4d39197e4bb570872b3e37d5
Jack Riley <jriley9000@gmail.com>
2020-05-30 12:07:49 -0700

    changed the database path

--- app.py
+++ app.py
@@ -1710,6 +1710,6 @@ if not app.debug:
 # Default port: want to switch this
 if __name__ == '__main__':
     #app.secret_key = 'xxxxyyyyyzzzzz'
-    app.run(host='0.0.0.0', threaded=True)
+    app.run(host='0.0.0.0', port=5743, threaded=True)
 
 # Or specify port manually:
