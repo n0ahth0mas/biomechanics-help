21a3625491a8dadac2afa9f02d846fe34ffdc6c4
Jack Riley <jriley9000@gmail.com>
2020-06-09 10:40:35 -0700

    swapping hardcoded key for env var

--- app.py
+++ app.py
@@ -100,7 +100,7 @@ def github_webhook_endpoint():
     if not signature or not signature.startswith("sha1="):
         abort(400, "X-Hub-Signature required")
     # Create local hash of payload
-    digest = hmac.new("help123".encode(),
+    digest = hmac.new(str(os.environ['REPOSITORY_KEY']).encode(),
                       request.data, hashlib.sha1).hexdigest()
 
     # Verify signature
